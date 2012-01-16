8a71a225b5b6bc7877041bdd40aa7c72a2e6ea45
myatsumoto <myatsumoto@gmail.com>
2012-01-16 14:19:53 +0900

    bootstrap.rb

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,12 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'rack'
-require 'sinatra/reloader' if development?
 require 'yaml'
 require 'json'
 require 'haml'
+
 [:helpers, :models ,:controllers].each do |dir|
-  Dir.glob(File.dirname(__FILE__)+"/#{dir}/*.rb").each do |rb|
+  Dir.glob(File.dirname(__FILE__)+"/app/#{dir}/*.rb").each do |rb|
     puts "loading #{rb}"
     require rb
   end
@@ -15,7 +15,7 @@ end
 set :haml, :escape_html => true
 
 begin
-  @@conf = YAML::load open(File.dirname(__FILE__)+'/config.yaml').read
+  @@conf = YAML::load open(File.dirname(__FILE__)+'/resource/conf/config.yaml').read
   p @@conf
 rescue => e
   STDERR.puts 'config.yaml load error!'
