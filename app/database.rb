7c7696b8aef8be1266440c8800462107add8f6a2
myatsumoto <myatsumoto@gmail.com>
2012-01-23 19:38:13 +0900

    database setting

--- app/database.rb
+++ app/database.rb
@@ -4,10 +4,14 @@ require 'mysql'
 
 class Database
   def initialize
-    @mysql = { :user => @@conf['username'], :password => @@conf['password'], :database => @@conf['database'] }
+    @mysql = {
+      :username => @@conf['username'],
+      :password => @@conf['password'],
+      :database => @@conf['database'] 
+    }
     @db = Mysql.init
     @db.options(Mysql::SET_CHARSET_NAME, 'utf8')
-    @db = Mysql::connect(@mysql[:localhost], @mysql[:user], @mysql[:password], @mysql[:database])
+    @db = Mysql::connect(@mysql[:localhost], @mysql[:username], @mysql[:password], @mysql[:database])
   end
 end
 
